8ae719e8d148cc0301c0aa260b484e5aba4116e9
showjoy <showjoy@192.168.0.100>
2017-07-09 10:58:04 +0800

    remove log

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
     let completion = vscode.languages.registerCompletionItemProvider(['vue', 'html'], completionItemProvider, '', ' ', ':', '<', '"', '/', '@');
 
     let disposable = vscode.commands.registerCommand('element-helper.searchUnderCursor', () => {
-        console.log(context.workspaceState.get('element-helper.loading', false));
         if (context.workspaceState.get('element-helper.loading', false)) {
             vscode.window.showInformationMessage('Document is initializing, please wait a minute.');
             return;
