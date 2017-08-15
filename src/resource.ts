355fcc682cb5cb8b4c32277e4960051e51ca76d1
showjoy <showjoy@showjoydeMacBook-Pro.local>
2017-08-15 16:49:46 +0800

    enhance code

--- src/resource.ts
+++ src/resource.ts
@@ -1,10 +1,10 @@
 'use strict';
 
-import { http as Http } from "follow-redirects";
 import { mkdir } from 'shelljs';
 const fs  = require('fs');
 const Path = require('path');
 const cheerio = require("cheerio");
+const http = require('http');
 
 export default class Resource {
   // resources local path
@@ -28,12 +28,23 @@ export default class Resource {
 
   static getFromUrl(url: string, filename?: string) {
     return new Promise((resolve, reject) => {
-      Http.get(url, response => {
-        response.on('error', reject);
-        let buffer = '';
-        response.on('data', chunk => { buffer += chunk; });
-        response.on('end', () => { resolve(buffer); });
-      }).on('error', reject);
+      http.get(url, (res) => {
+        const { statusCode } = res;
+        let error;
+        if (statusCode !== 200) {
+          error = new Error(`Request failure, status code: ${statusCode}`);
+        }
+        if (error) {
+          res.resume();
+          return reject(error.message);
+        }
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => resolve(rawData));
+      }).on('error', (e) => {
+        reject(`error: ${e.message}`);
+      });
     }).then(result => {
       if (filename) {
         mkdir('-p', Path.dirname(filename));
@@ -50,7 +61,11 @@ export default class Resource {
         const matched = [];
         content = content.replace(Resource.URL_REG, (match, one, two, three)=> {
           const name = Path.basename(three);
-          Resource.getFromUrl(`http:${three}`, Path.join(Path.dirname(htmlPath), name));
+          const url = `http:${three}`;
+          Resource.getFromUrl(url, Path.join(Path.dirname(htmlPath), name)).catch(error =>{
+            // one more again
+            Resource.getFromUrl(url, Path.join(Path.dirname(htmlPath), name));
+          });
           return `${one}${two}${name}${two}`;
         });
 
